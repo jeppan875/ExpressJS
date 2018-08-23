d963d107d3f1027bbcef133cda9ce1d6b2584e9c
jeppan875 <jeppan875@gmail.com>
2018-08-23 13:12:16 +0200

    j

--- myHomePage/routes/studies.js
+++ myHomePage/routes/studies.js
@@ -4,13 +4,28 @@ const Studies = require('../models/studies')
 
 router.get('/', (req, res) => {
   Studies.find({}, (err, studies) => {
-    console.log('studies')
+    studies.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+      return new Date(b.endDate) - new Date(a.endDate)
+    })
     if (err) {
       console.log(err)
-    } else {  
+    } else {
       res.render('studies', {
-        name: studies[0].name,
-        description: studies[0].description
+        studies
+      })
+    }
+  })
+})
+
+router.get('/:id', function (req, res) {
+  Studies.findById(req.params.id, function (err, studies) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('course', {
+        studies
       })
     }
   })
